8f8f2dc346d9e6147ae49cedd850815be5473f63
Adel <adel.bouzahar13@gmail.com>
2022-05-09 16:21:11 +0200

    Début du projet

---
+++ td-1.js
@@ -0,0 +1,30 @@
+const txtToAdd = document.querySelector('#txt_to_add');
+const pList = document.querySelector('#myList p');
+const pArray = document.querySelector('#myArray p');
+const myArray = ['une ligne', 'autre chose', 'et encore'];
+
+
+function AddText(){
+    document.querySelector('#myList p').innerText = document.getElementById('txt_to_add').value;y
+}
+
+
+function AddText1(){
+    if(txtToAdd.value!=='')
+    {
+        pList.innerText += txtToAdd.value+'\n';
+        // pList.innerText = pList.innerText + txtToAdd.value+'\n'
+        txtToAdd.value = '';
+    }
+    else{
+        alerte('Veuillez entrer un texte');
+    }
+    txtToAdd.focus();
+}
+
+
+function updateArray(){
+for(let i=0; i< myArray.length; i++){
+    pArray.innerText += myArray[i]+'\n';
+    }
+}
